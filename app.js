48ee0668ed8b3d927a86e84a18238a4a192dc1ec
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-09-26 09:27:46 +0530

    feat : initial express setup

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const router = require("./routes/userRouter");
+const session = require('express-session');
+const seed = require('./seedSecrets');
+require("dotenv").config();
+const db = require("./db/queries");
+
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use("/", router.userRouter);
+app.use(db.errorHandler);
+
+seed.hashSecret();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log("Listening on port now.");
+})
