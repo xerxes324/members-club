95dd8b347f10a00045294398cf80e054dc79966a
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-09-29 20:16:50 +0530

    feat : routes setup for all paths

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,28 @@
 const {Router} = require('express');
 const userRouter = Router();
 const userController = require("../controllers/userController");
-// const ac = require("../controllers/authenticationController");
 const ac = require("../middleware/authentication");
 const db = require("../db/queries");
 const pc = require("../controllers/passportConfig");
+const msg = require("../controllers/messages");
+const passport = require('passport');
 
 userRouter.get("/", userController.home);
-userRouter.get("/login", userController.login);
+userRouter.get("/login", ac.isLoggedin, userController.login);
 userRouter.get("/signup", userController.signup);
+userRouter.get("/dashboard", 
+    pc.ensureLogin,
+    msg.getMessages,
+    msg.memberAccess,
+    userController.dashboard
+);
+userRouter.get("/home", userController.home);
+userRouter.get("/logout", pc.ensureLogout, pc.userLogout);
+userRouter.get("/clubadmin", pc.ensureLogin, ac.isAdmin, userController.clubadmin);
+
+userRouter.post("/clubadmin",
+    ac.checkAdminPwd
+)
 
 userRouter.post("/signup",
     ac.validatePassword,
@@ -21,4 +35,34 @@ userRouter.post("/signup",
     db.createMember,
     pc.autoLogin
     );
+
+
+userRouter.post("/login",
+    passport.authenticate("local",
+        {
+            successRedirect:"/dashboard",
+            failureRedirect:"/login",
+        }
+    )
+);
+
+
+userRouter.loginErrorHandler = (err,req,res,next) =>{
+    res.render("login", {error : err.message});
+}
+
+userRouter.use(userRouter.loginErrorHandler);
+
+userRouter.post("/deleteMessage",
+    db.deleteMessage
+);
+
+userRouter.post("/dashboard",
+    db.addMessage
+);
+
+userRouter.get("{*splat}",
+    userController.fallback
+);
+
 module.exports = {userRouter}
