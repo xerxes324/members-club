89bf3f730be00463deef8f412306f0f0bfdd432c
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-09-29 20:33:58 +0530

    fix: removed redundant await in code

--- seedSecrets.js
+++ seedSecrets.js
@@ -3,8 +3,8 @@ const bcrypt = require("bcryptjs");
 const controller = require("./controllers/storePasscodes");
 
 exports.hashSecret = async() => {
-    const memberhash = await bcrypt.hash(process.env.MEMBERSHIP_PASSCODE, 10);
-    const adminhash = await bcrypt.hash(process.env.ADMIN_PASSCODE, 10);
+    const memberhash = bcrypt.hash(process.env.MEMBERSHIP_PASSCODE, 10);
+    const adminhash = bcrypt.hash(process.env.ADMIN_PASSCODE, 10);
     await controller.storePasscode(memberhash, adminhash);
 }
 
